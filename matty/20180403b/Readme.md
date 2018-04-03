5238db06e196c7d4d92e8514f051ba3f1b0b2842
kelu124 <kelu124@gmail.com>
2018-04-03 22:33:13 +0200

    New Matty test

---
+++ matty/20180403b/Readme.md
@@ -0,0 +1,17 @@
+# `20180403b` - New code for the board
+
+## Resources
+
+* [Binary file](/matty/prog_flash/pMATTYtestRegisterasyn_nomoreadd_20180401.bin)
+* [Client side](/matty/20180403b/20180403b-Client.ipynb)
+* [Server side](/matty/20180403b/20180403b-Server.ipynb)
+
+## Datasets
+
+* [Dataset](/matty/20180403b/p_A-130us-0-VGA@0x22-spimode1-64msps.csv)
+
+![](/matty/20180403b/p_A-130us-0-VGA@0x22-spimode1-64msps.jpg)
+
+* [Dataset v2](/matty/20180403b/p_A-130us-1-VGA@0x22-spimode1-64msps.csv)
+
+![](/matty/20180403b/p_A-130us-1-VGA@0x22-spimode1-64msps.jpg)
---
+++ matty/20180403b/mkimg.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -------------------------
+# (c) kelu124
+# cc-by-sa/4.0/
+#
+# Library for tagging image
+# Used in `20180310a`
+# Definitely not optimized
+#
+# -------------------------
+
+'''Description: Autotagging images for the `20180310a` experiment.'''
+
+__author__      = "kelu124"
+__copyright__   = "Copyright 2018, Kelu124"
+__license__ 	= "cc-by-sa/4.0/" 
+
+
+import sys
+import pyexiv2
+import os
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+Imgs = []
+for dirpath, dirnames, filenames in os.walk("."):
+    for filename in [f for f in filenames if ( f.endswith(".jpg") or f.endswith(".png") )]:
+        Imgs.append( os.path.join(dirpath, filename) )
+
+print Imgs
+
+for FileName in Imgs:
+	edit = 0
+
+	metadata = pyexiv2.ImageMetadata(FileName)
+	try:
+		metadata.read()
+	except IOError:
+		print "Not an image"
+	else:
+		# Modules
+		metadata['Exif.Image.Software'] = "matty"
+		# Experiment
+		metadata['Exif.Image.Make'] = "20180403b"
+
+
+		metadata['Exif.Photo.MakerNote'] = "graph"
+		# Description
+		metadata['Exif.Image.ImageDescription'] = "Testing new version of FPGA bin"
+
+		# Saving the image
+
+		metadata.write()
+		print "Image "+FileName+" saved"
+
